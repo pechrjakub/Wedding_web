0ce487ce0fe4d36676e360aa2544bd6e36fea1d3
Kuba <pechrjakub@gmail.com>
2026-06-14 22:12:25 +0200

    Úprava hero sekce + responzitivity

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -5,7 +5,6 @@ function Hero(){
           <h1>Karolína & Jakub</h1>
           <p>26 - 09 - 26</p>
         </div>
-        <a href="#intro" className="hero_scroll">↓</a>
       </section>
     );
 }
