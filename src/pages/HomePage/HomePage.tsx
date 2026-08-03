c23e15673cbc5c060de79eeffccbd89912496e36
KubaPrace <pechrjakub@gmail.com>
2026-08-03 15:24:02 +0200

    Added FAQ

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -7,6 +7,7 @@ import Dresscode from '../../components/dresscodeComponents/Dresscode';
 import Schedule from '../../components/scheduleComponents/Schedule';
 import Place from '../../components/Place';
 import Map from '../../components/Map';
+import FAQ from '../../components/FAQ';
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
       <Map />
       <Dresscode />
       <Schedule />
+      <FAQ />
       <Form />
     </main>
   );
